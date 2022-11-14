d55f15c11843e8315ae5a26caf8a136a072eab53
Andrew <aforest41@yahoo.com>
2022-11-14 00:02:17 -0800

    commit tree 63745e1a93ea21e31c48f1e6c111bbdcc1a7058f

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your code here!
+/* // Write your code here!
+const element = document.createElement("div");
+
+document.body.append(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+  }
+
+element.append(ul); */
+
+/* //Add Elements to the DOM via innerHTML, bad example, adding text in long string is error prone/ difficult to debug
+const main = document.getElementById("main");
+main.innerHTML =
+  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+  element.textContent = "You've changed what's on the screen!";
+
+element.style.fontSize = "24px";
+element.style.marginLeft = "30px";
+element.style.lineHeight = 2;
+
+element.className = "pet-listing dog"; */
+
+const main = document.getElementById("main");
+
+main.id = "new";
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Andrew is the champion";
+document.body.append(newHeader);
